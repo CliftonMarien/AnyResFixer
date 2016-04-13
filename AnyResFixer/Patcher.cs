ae67b7644e4538b3ec9b1733efdeda7b4a406dc6
Clifton M <Clifton@Cliftons-MacBook.local>
2016-04-12 21:54:13 -0400

    initial commit

---
+++ AnyResFixer/Patcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace AnyResFixer
+{
+	[KSPAddon(KSPAddon.Startup.EveryScene, false)]
+	public class Patcher : MonoBehaviour
+	{
+
+		void Start () {
+
+			GameSettings.SCREEN_RESOLUTION_HEIGHT = 720;
+			GameSettings.SCREEN_RESOLUTION_WIDTH = 1280;
+			GameSettings.FULLSCREEN = false;
+			GameSettings.SaveSettings ();
+			Screen.SetResolution(1280, 720, false);
+
+			Debug.Log ("AnyRes Patcher: Fixed screen resolution.");
+
+			PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Screen resolution fixed.", "AnyRes screen resolution fixed.  Please uninstall this plugin.  (Located at GameData/AnyResFixer.dll)", "OK", false, HighLogic.UISkin);
+
+		}
+
+	}
+}
+
